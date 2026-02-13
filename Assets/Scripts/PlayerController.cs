23c0a4ab92ab393bb2a185e992fd125c17cc0c85
João Nemezio <joaogabrieldl30@gmail.com>
2026-02-13 18:43:44 -0300

    refatorando move() e adicionando sistema basico de dash

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -5,27 +6,77 @@ public class PlayerController : MonoBehaviour
     
     public float velocidade;
     public bool podeMover = true;
+    public float forçadash = 20;
+    public Vector2 movimento;
     public Rigidbody2D rig;
+
+    public Vector2 ultimadireção;
+    private bool querDash;
 
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-       mover();
+        movimento = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (movimento != Vector2.zero)
+        {
+            ultimadireção = movimento.normalized;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            querDash = true;
+        }
+
+
+
+
+
     }
+
+    void FixedUpdate()
+    {
+        mover();
+
+        if (querDash)
+        {
+           dash();
+           querDash = false; 
+        }
+        
+    }
+
 
     void mover()
     {
-        if (podeMover)
-        {
-            Vector2 movimento = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-            rig.linearVelocity = movimento.normalized * velocidade;
-            
-        }
+        rig.linearVelocity = movimento * velocidade;
     }
 
-    
+    void dash()
+    {
+        
+        
+        if (movimento != Vector2.zero)
+        {
+            rig.AddForce(movimento.normalized * forçadash, ForceMode2D.Impulse);
+        }
+        else if (ultimadireção != Vector2.zero)
+        {
+             rig.AddForce(ultimadireção * forçadash, ForceMode2D.Impulse);
+        }
+        else
+        {
+            rig.AddForce(Vector2.up * forçadash, ForceMode2D.Impulse);
+        }
+        
+        
+        
+    }
+
+
+
 }
