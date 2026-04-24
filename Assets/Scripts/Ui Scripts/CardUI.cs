e0a9693010387babc65a6c5b42a68c0d8392a3cd
João Nemezio <joaogabrieldl30@gmail.com>
2026-04-24 14:13:22 -0300

    atualizar vida em buffs

--- Assets/Scripts/Ui Scripts/CardUI.cs
+++ Assets/Scripts/Ui Scripts/CardUI.cs
@@ -5,7 +5,17 @@ public class CardUI : MonoBehaviour
     public TextMeshProUGUI nomeTexto;
     private Artefato artefato;
     private CardSelectionUI manager;
-        public void Setup(Artefato novoArtefato, CardSelectionUI m)
+    private PlayerVida pv;
+    
+
+    void Start()
+    {
+
+        pv = FindAnyObjectByType<PlayerVida>();
+        
+    }
+    
+    public void Setup(Artefato novoArtefato, CardSelectionUI m)
     {
         artefato = novoArtefato;
         manager = m;
@@ -15,6 +25,7 @@ public class CardUI : MonoBehaviour
         public void OnClick()
     {
         manager.SelecionarArtefato(artefato);
+        pv.definirVida();
     }
 
 
