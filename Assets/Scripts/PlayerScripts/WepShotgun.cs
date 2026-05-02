a9ed88b70db1c68f4c2eabb12c54325d6b06b6c5
celsovssilva <celsoviniciussouzasilva@gmail.com>
2026-05-02 15:23:49 -0300

    sistema de troca de armas, troca na letra T, pistola pra doze

---
+++ Assets/Scripts/PlayerScripts/WepShotgun.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class WepShotgun : MonoBehaviour
+{
+    [Header("Configurações da Doze")]
+    public int quantidadePelotas = 5;
+    public float anguloAbertura = 30f; 
+    private float tempoProximoTiro;
+
+    [Header("Referências")]
+    public GameObject prefabTiro;
+    public Transform pontoInicialDoTiro;
+    public AudioClip fireSoundEffect;
+    
+    private PlayerController pc;
+    private SoundController soundController;
+
+    private void Start()
+    {
+       
+        pc = GetComponentInParent<PlayerController>();
+        soundController = pc.soundController;
+    }
+
+    private void Update()
+    {
+       
+        Vector3 mouseatq = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseatq.z = 0f;
+        Vector2 direcaoBase = (mouseatq - pontoInicialDoTiro.position).normalized;
+
+        pc.anim.SetFloat("mousePosHorizontal", direcaoBase.x);
+        pc.anim.SetFloat("mousePosVertical", direcaoBase.y);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Time.time >= tempoProximoTiro)
+            {
+                AtirarDoze(direcaoBase);
+               
+                tempoProximoTiro = Time.time + (pc.currentStatus.atqCooldown * 1.5f); 
+            }
+        }
+    }
+
+   void AtirarDoze(Vector2 direcaoPrincipal)
+    {
+        float anguloCentro = Mathf.Atan2(direcaoPrincipal.y, direcaoPrincipal.x) * Mathf.Rad2Deg;
+
+        for (int i = 0; i < quantidadePelotas; i++)
+        {
+            float variacao = (i - (quantidadePelotas - 1) / 2f) * (anguloAbertura / (quantidadePelotas - 1));
+            float anguloFinal = anguloCentro + variacao;
+
+            Vector2 direcaoTiro = new Vector2(
+                Mathf.Cos(anguloFinal * Mathf.Deg2Rad),
+                Mathf.Sin(anguloFinal * Mathf.Deg2Rad)
+            );
+
+            GameObject projetil = Instantiate(prefabTiro, pontoInicialDoTiro.position, Quaternion.identity);
+            projetil.GetComponent<Projetil>().definirDireção(direcaoTiro);
+
+          
+            Destroy(projetil, 0.2f); 
+           
+        }
+
+        if (soundController != null) soundController.TocarSom(fireSoundEffect);
+        pc.anim.SetTrigger("attack");
+    }
+}
