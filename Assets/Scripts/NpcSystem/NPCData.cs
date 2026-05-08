135c54d58e226ba38f79b80a6d43f3456e1c2f32
João Nemezio <joaogabrieldl30@gmail.com>
2026-05-08 00:55:32 -0300

    iniciando sistema de dialogo e loja

---
+++ Assets/Scripts/NpcSystem/NPCData.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCData : MonoBehaviour
+{
+    [Header("Info")]
+
+    public string nome;
+    public List<string> falas;
+    public List<string> falasPlayer;
+
+
+}
---
+++ Assets/Scripts/NpcSystem/NPController.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+
+public class NPController : MonoBehaviour
+{
+
+    [SerializeField] private NPCData nPCData;
+    
+    [SerializeField] private GameObject caixaDeDialogo;
+    [SerializeField] private GameObject interagirText;
+    [SerializeField] private CaixaDeDialogoUI caixaDeDialogoUI;
+    [SerializeField] private bool playerDentro;
+
+
+
+    void Start()
+    {
+        nPCData = GetComponent<NPCData>();
+        caixaDeDialogoUI = FindAnyObjectByType<CaixaDeDialogoUI>();
+
+
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F) && playerDentro)
+        {
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerDentro = true;
+            AtivarInfoInteragir();
+
+
+        
+        
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerDentro = false;
+            DesativarInfoInteragir();
+            
+        }
+    }
+
+
+
+
+
+
+    public void AtivarInfoInteragir()
+    {
+        interagirText.SetActive(true);
+
+    }
+
+
+    public void DesativarInfoInteragir()
+    {
+        interagirText.SetActive(false);
+
+    }
+
+
+
+
+
+
+
+
+
+}
