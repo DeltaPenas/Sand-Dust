f0f4e17cdfa9650b0fc3c28a0822181eca2f5790
João Nemezio <joaogabrieldl30@gmail.com>
2026-05-20 15:47:05 -0300

    Iniciando nova Ult

---
+++ Assets/Scripts/PlayerScripts/Ults/UltOrbital.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class UltOrbital : UltBase
+{
+    protected override bool TentaUsarUlt()
+    {
+        
+
+
+
+        
+      return true;
+    }
+
+    void SpawnarOrbital()
+    {
+        // Lógica para spawnar a órbita
+    }
+
+
+
+}
